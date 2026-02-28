3e8d33eda0f1c1df07679ea26b0caf0e09337371
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2026-02-28 14:05:07 +0600

    feat: redefine Drops model and establish relationships with Users, Purchases, and Reservations

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/db';
 
-export const Product = sequelize.define('Product', {
+export const Drops = sequelize.define('drop', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -10,8 +10,54 @@ export const Product = sequelize.define('Product', {
     type: DataTypes.FLOAT,
     defaultValue: 0,
   },
-  stock: {
+  totalStock: {
     type: DataTypes.INTEGER,
     defaultValue: 0,
   },
+  availableStock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0,
+    },
+  },
 });
+
+export const Users = sequelize.define('user', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export const Reservations = sequelize.define('reservation', {
+  status: {
+    type: DataTypes.ENUM,
+    values: ['reserved', 'purchased', 'expired'],
+  },
+  expiresAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+});
+
+export const Purchases = sequelize.define('Purchase', {
+  purchasedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+// A Drop has many Purchases
+Drops.hasMany(Purchases, { foreignKey: 'dropId' });
+Purchases.belongsTo(Drops, { foreignKey: 'dropId' });
+
+// A User has many Purchases
+Users.hasMany(Purchases, { foreignKey: 'userId' });
+Purchases.belongsTo(Users, { foreignKey: 'userId' });
+
+// Reservations link Users and Drops
+Users.hasMany(Reservations, { foreignKey: 'userId' });
+Drops.hasMany(Reservations, { foreignKey: 'dropId' });
+Reservations.belongsTo(Users, { foreignKey: 'userId' });
+Reservations.belongsTo(Drops, { foreignKey: 'dropId' });
