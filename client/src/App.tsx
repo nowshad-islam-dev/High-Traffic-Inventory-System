5f5715cbe070de5e262febfdebc1ee57c8d638b7
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2026-03-01 20:49:31 +0600

    feat: Add header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Toaster } from 'react-hot-toast';
 import { DropList } from './components/DropList';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
-      <h1 className='bg-slate-200 text-center text-3xl font-bold'>
-        Welcome to Sneaker Drop
-      </h1>
+      <Header />
+
       <DropList />
       <Toaster />
     </>
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div>
+      <div className='bg-slate-400 pr-4 rounded-md flex flex-row-reverse'></div>
+      <h1 className='bg-slate-200 text-center text-3xl font-bold'>
+        Welcome to Sneaker Drop
+      </h1>
+    </div>
+  );
+};
